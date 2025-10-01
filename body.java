3b95b159c17f7de03d1fafe74cc404c9ec371dd3
AidanAyerslol <aidanayers2009@gmail.com>
2025-10-01 11:54:14 -0400

    Created body class that will be used to represent the Sun, planets, moons, and orbital centers. All required variables and methods to return them are included in the class.

---
+++ body.java
@@ -0,0 +1,42 @@
+public abstract class body {
+    //required variables
+    //parent will be null if body is the sun
+    //angle, distance, velocity, and radius is 0 if the body is the sun
+    public String name;
+    private body parent;
+    private Double angle;
+    private Double distance;
+    private Double velocity;
+    private Double radius;
+
+    //constructor
+    public void createBody(String bodyname, body parentbody, Double relativeangle, Double relativedistance, Double orbitalvelocity, Double bodyradius){
+        name = bodyname;
+        parent = parentbody;
+        angle = relativeangle;
+        distance = relativedistance;
+        velocity = orbitalvelocity;
+        radius = bodyradius;
+    }
+
+
+    //functions to return variable data
+    public String getName(){
+        return name;
+    }
+    public body getParent(){
+        return parent;
+    }
+    public Double getAngle(){
+        return angle;
+    }
+    public Double getDistance(){
+        return distance;
+    }
+    public Double getVelocity(){
+        return velocity;
+    }
+    public Double getRadius(){
+        return radius;
+    }
+}
